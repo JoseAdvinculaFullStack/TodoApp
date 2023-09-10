b516b310a6328ac3bfb221709601ba93ae6b05b1
JoseAdvinculaFullStack <JoseAdvinculaFullStack@gmail.com>
2023-09-09 19:50:27 -0500

    arreglando segundo deploy

--- src/CheckButton/index.jsx
+++ src/CheckButton/index.jsx
@@ -4,7 +4,6 @@ import './CheckButton.css'
 import { TodoContext } from '../TodoContext'
 const CheckButton = ({active,id}) => {
     const {completeTodos,darkState} = useContext(TodoContext)
-    console.log(darkState)
     return(
         <button type="button" className={`CheckButton ${ active ? "CheckButton--active": ""} ${darkState ? 'CheckButton-dark' : ''}`} onClick={()=>completeTodos(id)}>
             <CheckSvg dark_value={darkState ? "true": "false"}/>
